3b94327140f20eab27761797cd3a2847d59004c6
VeraWatermelon <19730032635@163.com>
2025-11-09 08:54:30 +0800

    添加串口接收与发送功能

--- User/Mode1.c
+++ User/Mode1.c
@@ -1,12 +1,20 @@
 #include "stm32f10x.h"                  // Device header
+#include "Serial.h"
 #include "OLED.h"
 #include <stdio.h>
 #include "Key.h"
 uint8_t KeyNum;
 uint8_t Mode1(void)
 {
+	Serial_Init();
 	while(1)
 	{
+		if (Serial_RxFlag == 1)	
+		{
+			
+			
+			Serial_RxFlag = 0;
+		}
 		KeyNum=Key_GetNum();
 		if(KeyNum==1)
 		{
