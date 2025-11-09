1dc67c6795142177abeb92d41cf9884dee5caf06
VeraWatermelon <19730032635@163.com>
2025-11-09 11:41:40 +0800

    添加编码器测速和PID

---
+++ Hardware/Encoder.h
@@ -0,0 +1,8 @@
+#ifndef __Encoder_H
+#define __Encoder_H
+
+void Encoder1_Init(void);
+void Encoder2_Init(void);
+int16_t Encoder1_Get(void);
+int16_t Encoder2_Get(void);
+#endif
---
+++ System/Timer.h
@@ -0,0 +1,5 @@
+#ifndef __Timer_H
+#define __Timer_H
+
+void Timer1_Init(void);
+#endif
